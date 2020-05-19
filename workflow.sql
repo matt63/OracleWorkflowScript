bc10233692188a1a0e6926736c0c0180371ab949
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2020-05-19 11:46:45 +1000

    Update workflow.sql

--- workflow.sql
+++ workflow.sql
@@ -2,7 +2,7 @@
 WHENEVER OSERROR EXIT FAILURE
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
-/* Insert all your commands here.   */
+/* Insert all your commands here.   The example select command is just a place holder.    */
 SELECT * FROM user_users;
 
 /* Leave the exit in place so we exit cleanly */
